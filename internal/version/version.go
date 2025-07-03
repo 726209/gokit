a0d6579a5c022179a474f148b8953a9413ca083b
ioser <iospai@qq.com>
2025-07-03 11:34:39 +0800

    chore(release): 0.0.5-alpha.2

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 // Version is the current version of the CLI tool.
 // It is updated automatically by standard-version.
-var Version = "0.0.5-alpha.1"
+var Version = "0.0.5-alpha.2"
 
 func GetVersion() string {
 	return Version
